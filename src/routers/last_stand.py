27c94698797e710587d3919a17753e73f3ac3b88
Goldenbarnowl <danil.drodin@mail.ru>
2025-06-04 20:22:10 +0600

    v5.2

--- src/routers/last_stand.py
+++ src/routers/last_stand.py
@@ -1,11 +1,12 @@
 import asyncio
 
 from aiogram import Router, F
+from aiogram.filters import Command
 from aiogram.types import Message
 
-from config import bot, admin_group, users_data_repo, teacher_thread, parent_thread, pupil_thread
+from config import bot, admin_group, users_data_repo, teacher_thread, parent_thread, pupil_thread, supabase
 from phrases import LAST_STAND
-from src.keyboards.pupil_keyboard import event_info_keyboard
+from src.keyboards.pupil_keyboard import event_info_keyboard, events_keyboard
 from src.keyboards.user_keyboards import role_buttons
 from src.states.user_states import User
 
@@ -131,7 +132,56 @@ async def handle_event_end(call: Message):
     )
 
 
+@last_stand_router.message(Command('spamtest'), F.chat.id == 820176381)
+async def spam_attack(message: Message):
+    text = """🎉 Новые мероприятия в Омске!
+Мы обновили список событий — теперь в боте доступны свежие мероприятия, которые пройдут в Омске в ближайшее время!"""
+    response = supabase.table('UserData').select('*').execute()
+    data = response.data
+    counter = 0
+    await bot.send_message(chat_id=message.chat.id,
+                           text=text,
+                           parse_mode='HTML')
 
+    for userdata in data:
+        try:
+            await bot.send_photo(chat_id=userdata['chat_id'],
+                                 text=text,
+                                 parse_mode='HTML',
+                                 reply_markup=events_keyboard())
+            counter += 1
+        except Exception as e:
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=f"{counter} {userdata['chat_id']} - не отправлено\n {str(e)})",
+                                   parse_mode='HTML')
+
+    print('Отправлено:', counter)
+
+
+@last_stand_router.message(Command('spam'), F.chat.id == 820176381)
+async def spam_attack(message: Message):
+    text = """🎉 Новые мероприятия в Омске!
+Мы обновили список событий — теперь в боте доступны свежие мероприятия, которые пройдут в Омске в ближайшее время!"""
+    response = supabase.table('UserData').select('*').execute()
+    data = response.data
+    counter = 0
+    await bot.send_message(chat_id=message.chat.id,
+                           text=text,
+                           parse_mode='HTML')
+
+    for userdata in data:
+        try:
+            await bot.send_photo(chat_id=userdata['chat_id'],
+                                 text=text,
+                                 parse_mode='HTML',
+                                 reply_markup=events_keyboard())
+            counter += 1
+        except Exception as e:
+            await bot.send_message(chat_id=message.chat.id,
+                                   text=f"{counter} {userdata['chat_id']} - не отправлено\n {str(e)})",
+                                   parse_mode='HTML')
+
+    print('Отправлено:', counter)
 
 
 @last_stand_router.message(F.chat.id != admin_group)
@@ -141,4 +191,5 @@ async def handle_last_stand(message: Message):
         chat_id=chat_id,
         text=LAST_STAND
     )
+
 
